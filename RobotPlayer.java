066db448f3be5ae7e0a2f1eceb775b80cd046da8
Jedimaster4559 <nbsolomon4559@gmail.com>
2017-01-14 16:12:09 -0600

    started tree planting method

--- RobotPlayer.java
+++ RobotPlayer.java
@@ -386,6 +386,12 @@ public strictfp class RobotPlayer {
 
 		}
 
-}
-		
 
+		public static void plantCircle() throws GameActionException{
+			//try present location
+			if(rc.isCircleOccupiedExceptByThisRobot(rc.getLocation(),(2*GameConstants.BULLET_TREE_RADIUS)+(2*RobotType.GARDENER.bodyRadius))){
+				
+			}
+		
+		}
+}

881e83ebf87a31e91cdfd99eb6d517318f580940
Jedimaster4559 <nbsolomon4559@gmail.com>
2017-01-16 01:00:51 -0600

    added methods for John to write

---
+++ JohnMethods.java
@@ -0,0 +1,27 @@
+/**
+ * Take this method and anything with and *astrisk* think around it will need
+ * to be edited. Use the javadocs to help you as you go. You can do this in
+ * the actual document. Make sure that you are using individual methods for
+ * each. If you have any questions, feel free to call or text! 
+ * 
+ * Channel Reservations are in the Battlecode General Strategy Document
+ */
+
+
+/**
+ * Method to create *unit* while keeping a count of total created
+ * 
+ * @author John Boom
+ * @param test a test to determine if or when possible
+ * @throws GameActionException
+ * @version V1
+ */
+public static void create*Unit*(Boolean test) throws GameActionException{
+	if(test){
+		rc.build*unit*();
+		int i =rc.readBroadcast(*insert proper channel*);
+		int i++;
+		rc.broadcast(*insert proper channel*, i);
+	}
+	
+}
